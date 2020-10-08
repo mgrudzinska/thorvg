42a958317f78877a2947daada45a4aeaf2b96200
Mira Grudzinska <m.grudzinska@samsung.com>
2020-10-08 16:08:52 +0200

    examples: added an example that causes a crash
    The dashpattern with length and gap values < 1 causes a crash.

---
+++ src/examples/SmallDashPatterns.cpp
@@ -0,0 +1,157 @@
+#include "Common.h"
+
+/************************************************************************/
+/* Drawing Commands                                                     */
+/************************************************************************/
+
+void tvgDrawCmds(tvg::Canvas* canvas)
+{
+    if (!canvas) return;
+
+    auto shape1 = tvg::Shape::gen();
+    auto shape2 = tvg::Shape::gen();
+    auto shape3 = tvg::Shape::gen();
+    float dashPattern1[2] = {0.9, 0.9};      // almost solid (crashes)
+    float dashPattern2[2] = {0, 9};          // no plot
+    float dashPattern3[4] = {0, 0, 0.9, 0};  // like a solid line (crashes)
+
+    shape1->moveTo(100, 100);
+    shape1->lineTo(100, 300);
+    shape1->lineTo(300, 300);
+    shape1->lineTo(300, 100);
+    shape1->cubicTo(300, 50, 600, 50, 200, 200);
+    shape1->close();
+    shape1->stroke(255, 0, 0, 255);
+    shape1->stroke(1);
+    shape1->stroke(dashPattern1, 2);
+    if (canvas->push(move(shape1)) != tvg::Result::Success) return;
+
+    shape2->moveTo(350, 450);
+    shape2->lineTo(350, 650);
+    shape2->lineTo(550, 650);
+    shape2->lineTo(550, 450);
+    shape2->cubicTo(550, 400, 850, 400, 450, 550);
+    shape2->close();
+    shape2->stroke(0, 255, 0, 255);
+    shape2->stroke(2);
+    shape2->stroke(dashPattern2, 2);
+    if (canvas->push(move(shape2)) != tvg::Result::Success) return;
+
+    shape3->moveTo(400, 100);
+    shape3->lineTo(400, 300);
+    shape3->lineTo(600, 300);
+    shape3->lineTo(600, 100);
+    shape3->cubicTo(600, 50, 900, 50, 500, 200);
+    shape3->close();
+    shape3->stroke(0, 0, 255, 255);
+    shape3->stroke(4);
+    shape3->stroke(dashPattern3, 4);
+    if (canvas->push(move(shape3)) != tvg::Result::Success) return;
+}
+
+
+/************************************************************************/
+/* Sw Engine Test Code                                                  */
+/************************************************************************/
+
+static unique_ptr<tvg::SwCanvas> swCanvas;
+
+void tvgSwTest(uint32_t* buffer)
+{
+    //Create a Canvas
+    swCanvas = tvg::SwCanvas::gen();
+    swCanvas->target(buffer, WIDTH, WIDTH, HEIGHT, tvg::SwCanvas::ARGB8888);
+
+    /* Push the shape into the Canvas drawing list
+       When this shape is into the canvas list, the shape could update & prepare
+       internal data asynchronously for coming rendering.
+       Canvas keeps this shape node unless user call canvas->clear() */
+    tvgDrawCmds(swCanvas.get());
+}
+
+void drawSwView(void* data, Eo* obj)
+{
+    if (swCanvas->draw() == tvg::Result::Success) {
+        swCanvas->sync();
+    }
+}
+
+
+/************************************************************************/
+/* GL Engine Test Code                                                  */
+/************************************************************************/
+
+static unique_ptr<tvg::GlCanvas> glCanvas;
+
+void initGLview(Evas_Object *obj)
+{
+    static constexpr auto BPP = 4;
+
+    //Create a Canvas
+    glCanvas = tvg::GlCanvas::gen();
+    glCanvas->target(nullptr, WIDTH * BPP, WIDTH, HEIGHT);
+
+    /* Push the shape into the Canvas drawing list
+       When this shape is into the canvas list, the shape could update & prepare
+       internal data asynchronously for coming rendering.
+       Canvas keeps this shape node unless user call canvas->clear() */
+    tvgDrawCmds(glCanvas.get());
+}
+
+void drawGLview(Evas_Object *obj)
+{
+    auto gl = elm_glview_gl_api_get(obj);
+    gl->glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    gl->glClear(GL_COLOR_BUFFER_BIT);
+
+    if (glCanvas->draw() == tvg::Result::Success) {
+        glCanvas->sync();
+    }
+}
+
+
+/************************************************************************/
+/* Main Code                                                            */
+/************************************************************************/
+
+int main(int argc, char **argv)
+{
+    tvg::CanvasEngine tvgEngine = tvg::CanvasEngine::Sw;
+
+    if (argc > 1) {
+        if (!strcmp(argv[1], "gl")) tvgEngine = tvg::CanvasEngine::Gl;
+    }
+
+    //Initialize ThorVG Engine
+    if (tvgEngine == tvg::CanvasEngine::Sw) {
+        cout << "tvg engine: software" << endl;
+    } else {
+        cout << "tvg engine: opengl" << endl;
+    }
+
+    //Threads Count
+    auto threads = std::thread::hardware_concurrency();
+
+    //Initialize ThorVG Engine
+    if (tvg::Initializer::init(tvgEngine, threads) == tvg::Result::Success) {
+
+
+        elm_init(argc, argv);
+
+        if (tvgEngine == tvg::CanvasEngine::Sw) {
+            createSwView();
+        } else {
+            createGlView();
+        }
+
+        elm_run();
+        elm_shutdown();
+
+        //Terminate ThorVG Engine
+        tvg::Initializer::term(tvgEngine);
+
+    } else {
+        cout << "engine is not supported" << endl;
+    }
+    return 0;
+}
